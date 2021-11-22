57ac3c7d3b761ca3968960be60a90436a0b5cda2
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2021-11-22 16:26:24 +0100

    Push de fin de séance

--- client.js
+++ client.js
@@ -2,3 +2,39 @@
  * client.js
  */
 let socketClient = io();
+
+/*
+ * Emits an event to the server
+ *
+ * - The first parameter is the event name.
+ * - The second parameter is the message content: it can be a number,
+ *   a string or an object.
+ */
+socketClient.emit('hello', 'Olivier');
+
+/*
+ * Registers event listeners
+ *
+ * - The first parameter is the event name
+ * - The second parameter is a callback function that processes
+ *   the message content.
+ */
+socketClient.on('notification', (content) => {
+  console.log(content);
+});
+
+/*
+socketClient.on('hello', (content) => {
+  console.log(content);
+});
+*/
+
+let form = document.forms["signin"];
+let nickname = document.querySelector("input[name='nickname']");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (nickname.value){
+        socketClient.emit('>signin', nickname.value)
+    }
+});
--- server.js
+++ server.js
@@ -23,15 +23,56 @@ express.get('/client.js', (request, response) => {
         response.writeHead(200, { 'Content-Type': 'text/javascript' });
         response.end(content);
       })
-})
-
-
-// Binds a socket server to the current HTTP server
-let socketServer = require('socket.io')(http);
-// Registers an event listener ('connection' event)
-socketServer.on('connection', (socket) => {
-  console.log('A new user is connected...');
+      .catch((error) => {
+        // Returns 404 error: page not found
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('Page not found.');
+      });
 });
 
 // Server listens on port 8080
 http.listen(8080);
+
+// Binds a socket server to the current HTTP server
+let socketServer = require('socket.io')(http);
+// Registers an event listener ('connection' event)
+socketServer.on('connection', function (socket) {
+  console.log('A new user is connected...');
+
+/*
+   * Registers an event listener
+   *
+   * - The first parameter is the event name
+   * - The second parameter is a callback function that processes
+   *   the message content.
+   */
+  /*
+  socket.on('hello', (content) => {
+    console.log(content + ' says hello!');
+
+    // Pushes an event to all the connected clients
+    socketServer.emit('notification', content + ' says hello!');
+
+    // Pushes an event to the client related to the socket object
+    socket.emit('hello', 'Hi ' + content + ', wassup mate?');
+  });
+  */
+
+  let registeredSockets = {};
+
+  function isAvailable (nickname) {
+
+    // if (Object.keys(registeredSockets).includes(nickname))
+    if (registeredSockets[nickname] === undefined) {
+      return true;
+    }
+    return false;
+  }
+
+    socket.on('>signin', (content) => {
+      if (isAvailable(content)){
+        console.log('login :' + content);
+        Object.keys(registeredSockets).push(content);
+      } 
+    });
+});
